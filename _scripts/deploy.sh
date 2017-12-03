d358c8e7b63d7e164cce98655806cd140ab96316
Petr Hejna <Achse0@gmail.com>
2017-12-03 14:51:32 +0100

    deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo 'Fetching code...'
+git fetch origin
+git reset origin/master --hard
+
 echo 'Killing all coinrat...'
 ps aux | grep 'python -m coinrat' | tr -s ' ' | cut -d' ' -f2 | xargs kill -15
 
@@ -7,6 +11,6 @@ echo 'Clearing logs...'
 rm -fr logs/*
 rm -fr nohup.out
 
-echo 'Fetching code...'
-git fetch origin
-git reset origin/master --hard
+echo 'Starting new coinrat processes...'
+nohup python -m coinrat synchronize cryptocompare USD BTC &
+nohup python -m coinrat run_strategy double_crossover USD BTC bittrex &
