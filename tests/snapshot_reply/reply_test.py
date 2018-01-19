0c0a5333904214a709767066ef7f8e6b984ec0cd
Petr Hejna <Achse0@gmail.com>
2018-01-19 13:27:56 +0100

    replay spashot test working now

--- tests/snapshot_reply/reply_test.py
+++ tests/snapshot_reply/reply_test.py
@@ -3,7 +3,7 @@ import logging
 import os
 import datetime
 import uuid
-from typing import Dict
+from typing import Dict, List
 from uuid import UUID
 
 import pytest
@@ -12,16 +12,18 @@ from influxdb import InfluxDBClient
 
 from coinrat_influx_db_storage.candle_storage import CandleInnoDbStorage
 from coinrat_influx_db_storage.order_storage import OrderInnoDbStorage
-from coinrat.domain import Pair, FrozenDateTimeFactory
+from coinrat.domain import Pair, FrozenDateTimeFactory, DateTimeFactory, Strategy, Market
 from coinrat.domain.candle import CandleExporter
-from coinrat.domain.order import Order
 from coinrat_double_crossover_strategy.strategy import DoubleCrossoverStrategy
 from coinrat_mock.market import MockMarket
+from coinrat.domain.order import serialize_orders, OrderStorage
+from coinrat.domain.candle import CandleStorage
+from coinrat.event.event_emitter import EventEmitter
 
 DUMMY_MARKET = 'dummy_market'
 BTC_USD_PAIR = Pair('USD', 'BTC')
 
-uuid_mock_list = [UUID("3cf10055-68c2-43ad-b608-{0:012d}".format(number)) for number in range(1, 100)]
+UUID_MOCK_LIST = [UUID("3cf10055-68c2-43ad-b608-{0:012d}".format(number)) for number in range(1, 100)]
 
 logging.disable(logging.DEBUG)
 
@@ -51,65 +53,85 @@ def test_candle_ticks_are_stored(
     start: datetime.datetime,
     end: datetime.datetime
 ):
-    expectation = flexmock(uuid).should_receive('uuid4')
-    for uuid_item in uuid_mock_list:
-        expectation.and_return(uuid_item)
-
+    mock_uuid()
     dataset_path = os.path.dirname(__file__) + '/' + dataset
-
     candle_storage = CandleInnoDbStorage(influx_database)
+    import_candles_into_storage(dataset_path, candle_storage)
     order_storage = OrderInnoDbStorage(influx_database)
-
-    exporter = CandleExporter(candle_storage)
-    exporter.import_from_file(dataset_path + '/candles.json')
-
     datetime_factory = FrozenDateTimeFactory(start)
+    emitter_mock = create_emitter_mock()
 
-    emitter_mock = flexmock()
-    emitter_mock.should_receive('emit_new_candles')
-    emitter_mock.should_receive('emit_new_order')
+    strategy = create_strategy_to_test(candle_storage, order_storage, emitter_mock, datetime_factory)
+    mock_market = MockMarket(datetime_factory, {'mocked_market_name': 'bittrex'})
 
-    strategy_configuration = {
-        'long_average_interval': 60 * 60,
-        'short_average_interval': 15 * 60,
-        'delay': 0,
-    }
+    run_strategy_replay(strategy, mock_market, datetime_factory, end)
 
-    strategy = DoubleCrossoverStrategy(
-        candle_storage,
-        order_storage,
-        emitter_mock,
-        datetime_factory,
-        strategy_configuration
-    )
-    market_configuration = {'mocked_market_name': 'bittrex'}
-    mock_market = MockMarket(datetime_factory, market_configuration)
+    result_orders = get_created_orders_from_storage(order_storage)
+    save_last_result_into_file(dataset_path, result_orders)
+    assert_orders_against_expectation_file(dataset_path, result_orders)
 
-    while datetime_factory.now() < end:
-        strategy.tick([mock_market], BTC_USD_PAIR)
-        datetime_factory.move(datetime.timedelta(seconds=30))
 
-    orders = order_storage.find_by(market_name='bittrex', pair=BTC_USD_PAIR)
-    result_orders = list(map(_data_to_order, orders))
-
-    file = open(dataset_path + '/orders_last_run.json', 'w')
-    file.write(json.dumps(result_orders))
-    file.close()
-
+def assert_orders_against_expectation_file(dataset_path, result_orders: List[Dict]) -> None:
     with open(dataset_path + '/orders.json') as json_file:
         expected_orders = json.load(json_file)
         assert result_orders == expected_orders
 
 
-def _data_to_order(order: Order) -> Dict:
-    return {
-        "time": order.created_at.isoformat(),
-        "direction": order._direction,
-        "market": order.market_name,
-        "order_id": str(order.order_id),
-        "pair": str(order.pair),
-        "quantity": str(order.quantity),
-        "rate": str(order.rate),
-        "status": order._status,
-        "type": order.type
-    }
+def get_created_orders_from_storage(order_storage: OrderStorage) -> List[Dict]:
+    orders = order_storage.find_by(market_name='bittrex', pair=BTC_USD_PAIR)
+    result_orders = serialize_orders(orders)
+    return result_orders
+
+
+def run_strategy_replay(
+    strategy: Strategy,
+    mock_market: Market,
+    datetime_factory: FrozenDateTimeFactory,
+    end: datetime.datetime
+):
+    while datetime_factory.now() < end:
+        strategy.tick([mock_market], BTC_USD_PAIR)
+        datetime_factory.move(datetime.timedelta(seconds=30))
+
+
+def create_strategy_to_test(
+    candle_storage: CandleStorage,
+    order_storage: OrderStorage,
+    emitter_mock: EventEmitter,
+    datetime_factory: DateTimeFactory
+):
+    return DoubleCrossoverStrategy(
+        candle_storage,
+        order_storage,
+        emitter_mock,
+        datetime_factory,
+        {
+            'long_average_interval': 60 * 60,
+            'short_average_interval': 15 * 60,
+            'delay': 0,
+        }
+    )
+
+
+def import_candles_into_storage(dataset_path: str, candle_storage: CandleStorage) -> None:
+    exporter = CandleExporter(candle_storage)
+    exporter.import_from_file(dataset_path + '/candles.json')
+
+
+def mock_uuid() -> None:
+    expectation = flexmock(uuid).should_receive('uuid4')
+    for uuid_item in UUID_MOCK_LIST:
+        expectation.and_return(uuid_item)
+
+
+def create_emitter_mock() -> EventEmitter:
+    emitter_mock = flexmock()
+    emitter_mock.should_receive('emit_new_candles')
+    emitter_mock.should_receive('emit_new_order')
+    return emitter_mock
+
+
+def save_last_result_into_file(dataset_path: str, result_orders: List[Dict]) -> None:
+    file = open(dataset_path + '/orders_last_run.json', 'w')
+    file.write(json.dumps(result_orders))
+    file.close()
