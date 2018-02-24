120ded68c48ad407fefb8da5aa8034fdd27a7b39
Petr Hejna <Achse0@gmail.com>
2018-02-24 16:33:18 +0100

    add test for previsous commit

--- coinrat_bittrex/test/test_bittrex_synchronizer.py
+++ coinrat_bittrex/test/test_bittrex_synchronizer.py
@@ -3,6 +3,7 @@ import datetime
 from decimal import Decimal
 from flexmock import flexmock
 
+from coinrat.domain.market import MarketException
 from coinrat_bittrex.synchronizer import BittrexSynchronizer
 from coinrat.domain.pair import Pair
 from coinrat.domain.candle import Candle
@@ -29,6 +30,18 @@ def test_synchronize_success():
     storage.should_receive('write_candles').times(2)
 
     synchronizer = BittrexSynchronizer(market, storage, create_emitter_mock(), delay=0, number_of_runs=1)
+    synchronizer.synchronize(BTC_USD_PAIR)
+
+
+def test_synchronize_recover_from_error():
+    market = flexmock(name='yolo_market')
+    market.should_receive('get_candles').and_return([]).times(2)
+    market.should_receive('get_last_minute_candles').and_raise(MarketException()).times(1)
+
+    storage = flexmock(name='yolo_storage')
+    storage.should_receive('write_candles')
+
+    synchronizer = BittrexSynchronizer(market, storage, create_emitter_mock(), delay=0, number_of_runs=3)
     synchronizer.synchronize(BTC_USD_PAIR)
 
 
